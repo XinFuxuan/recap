fcb8290e75ab5be01a9af16ec093637d5ebf30e4
XinFuxuan <x7182000@yahoo.com>
2021-06-30 19:42:34 -0400

    recap practice add more

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("recapping like day 1, i wanted to recap more");
+
     }
+
 }
